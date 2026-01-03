78877cc34260d7dd2e260dd770e5245388aca58b
Matthew <matthew.holmes111@gmail.com>
2026-01-03 15:04:58 +0000

    plan orchestration work

--- SyntheticPDFs/Logic/Orchestrator.Work.cs
+++ SyntheticPDFs/Logic/Orchestrator.Work.cs
@@ -9,6 +9,17 @@
         {
 
             _logger.LogInformation("Work commencing");
+
+
+            // pull git (use git repo manager)
+            // get repo model (from git repo manager?)
+            // decide what to generate (get this in separate file for business logic clarity)
+            // create synthetic Tex source
+            // pull git again - check nothing has changed
+            // commit and push if can
+            // backoff if push failure
+
+
             // Simulate real work
             await Task.Delay(TimeSpan.FromSeconds(10));
         }
