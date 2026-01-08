509e32acdcf1276514573829fc04dabeefb482d8
Matthew <matthew.holmes111@gmail.com>
2026-01-08 17:28:47 +0000

    repo manager can push to git

--- SyntheticPDFs/Models/TexSourceModel.cs
+++ SyntheticPDFs/Models/TexSourceModel.cs
@@ -1,6 +1,13 @@
 ﻿namespace SyntheticPDFs.Models
 {
-    public class TexSourceModel
+    public record TexSourceModel
     {
+        public required String TexSource { get; init; }
+        public String FileNameFullPath => DirNoFileName + "/" + FileNameNoPathNoExt + ".tex";
+
+        
+        public required String FileNameNoPathNoExt { get; init; }
+        public required String DirNoFileName { get; init; }
+
     }
 }
