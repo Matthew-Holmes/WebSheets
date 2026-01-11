46245b8bb253a2c70a272b817deeb19540c7d7b1
Matthew <matthew.holmes111@gmail.com>
2026-01-11 14:27:21 +0000

    snapshot

--- SyntheticPDFs/Logic/Orchestrator.PromptTemplates.cs
+++ SyntheticPDFs/Logic/Orchestrator.PromptTemplates.cs
@@ -2,9 +2,6 @@
 {
     public partial class Orchestrator
     {
-        private static String GenerateEnglishWorkedSolutionsPrompt(String rootSourceContents)
-        {
-            return $"Below is the contents of a .tex file. Typeset worked solutions in LaTeX, showing clear workings with explanations, just provide the contents of the worked solutions .tex file, nothing else, it MUST compile first time. Original source: \n\n {rootSourceContents}";
-        }
+
     }
 }
